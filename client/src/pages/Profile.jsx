db3d5697cdb11c240839b89b171c1908d5a4950e
Lenny-Opicha <ochangoopicha@gmail.com>
2026-03-12 23:17:25 +0300

    added Profile page structure

---
+++ client/src/pages/Profile.jsx
@@ -0,0 +1,23 @@
+function Profile(){
+
+  return(
+
+    <div>
+
+      <h2>My Profile</h2>
+
+      <h3>My Items</h3>
+
+      <p>No items posted yet</p>
+
+      <h3>Account Settings</h3>
+
+      <p>Phone number and location details will appear here.</p>
+
+    </div>
+
+  )
+
+}
+
+export default Profile 
