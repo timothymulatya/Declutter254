0a53ba2a2d946f049566a0b79643b763a52db668
Lenny-Opicha <ochangoopicha@gmail.com>
2026-03-12 23:41:25 +0300

    created RequestActions component with approve and reject buttons

---
+++ client/src/components/RequestActions.jsx
@@ -0,0 +1,47 @@
+import { approveRequest, rejectRequest } from "../api/items"
+
+function RequestActions({ request, refreshRequests }){
+
+  function handleApprove(){
+
+    approveRequest(request.id)
+    .then(() => refreshRequests())
+
+  }
+
+  function handleReject(){
+
+    rejectRequest(request.id)
+    .then(() => refreshRequests())
+
+  }
+
+  if(request.status !== "pending"){
+    return null
+  }
+
+  return(
+
+    <div style={{marginTop:"10px"}}>
+
+      <button
+        onClick={handleApprove}
+        style={{marginRight:"10px", background:"green", color:"white"}}
+      >
+        Approve
+      </button>
+
+      <button
+        onClick={handleReject}
+        style={{background:"red", color:"white"}}
+      >
+        Reject
+      </button>
+
+    </div>
+
+  )
+
+}
+
+export default RequestActions 
